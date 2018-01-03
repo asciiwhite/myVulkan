6774e2c203a3f58d55c6cf9ebe3c3477f7fbb4c5
Christian Vix <christian_vix@mentor.com>
2018-01-03 13:28:40 +0100

    Added missing handles.h

---
+++ src/vulkan/handles.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <memory>
+
+#define DECLARE_HANDLE(Object) \
+    class Object; \
+    using Object##Handle = std::shared_ptr<Object>;
+
+DECLARE_HANDLE(Shader);
+DECLARE_HANDLE(Texture);
+DECLARE_HANDLE(Pipeline);
